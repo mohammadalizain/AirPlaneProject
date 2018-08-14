5b07ecd2acc425a38d0f5e90cd8bd4989481583e
mohammad.h.ali@kw.zain.com <mohammad.h.ali@kw.zain.com>
2018-08-14 06:12:09 -0700

    Updated Phase 2

--- AirPlane_Hanger/urls.py
+++ AirPlane_Hanger/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from airplanes.views import list_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', list_view),
 ]
 
 
--- airplanes/views.py
+++ airplanes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import airplanes
 # Create your views here.
+
+def list_view(request):
+    objects = airplanes.objects.all()
+    context = {
+        "object_list": objects,
+    }
+    return render(request, 'list_page.html', context)
